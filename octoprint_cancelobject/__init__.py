5548132417e66a32d729807401edf6f367b6ddbe
paukstelis <paukstel@umd.edu>
2019-02-09 23:33:57 -0500

    First functional version of gcodeviewer cancelling

--- octoprint_cancelobject/__init__.py
+++ octoprint_cancelobject/__init__.py
@@ -56,10 +56,11 @@ class Gcode_parser:
         Match given line against extrusion move regex
         :param line: g-code line
         :return: None or tuple with X, Y and E positions
+        Modified to only return true with positive extrusion
         """
         self.last_match = None
         m = self._parse_move_args(line)
-        if m and (m[0] is not None or m[1] is not None) and m[3] is not None and m[3] != 0:
+        if m and (m[0] is not None or m[1] is not None) and m[3] is not None and m[3] > 0:
             self.last_match = m
         return self.last_match
 
@@ -188,7 +189,8 @@ class CancelobjectPlugin(octoprint.plugin.StartupPlugin,
         return dict(
             skip=[],
             cancel=["cancelled"],
-            objlist=[]
+            objlist=[],
+            resetpos=[]
         )
 
     def on_api_command(self, command, data):
@@ -202,7 +204,18 @@ class CancelobjectPlugin(octoprint.plugin.StartupPlugin,
             self._cancel_object(cancelled)
             
         if command == "objlist":
-            return flask.make_response(flask.jsonify(dict(list=self.object_list)))
+        	#This is for 
+        	self._updateobjects()
+        	#This is for the iOS plugin
+        	response = flask.make_response(flask.jsonify(dict(list=self.object_list)))
+        	return response
+        	
+        if command == "resetpos":
+        	for obj in self.object_list:
+        		obj["max_x"] = 0;
+        		obj["max_y"] = 0;
+        		obj["min_x"] = 10000;
+        		obj["min_y"] = 10000;
 
     #Is this really needed?
     def on_api_get(self, request):
@@ -239,6 +252,7 @@ class CancelobjectPlugin(octoprint.plugin.StartupPlugin,
             self._plugin_manager.send_plugin_message(self._identifier, dict(objects=self.object_list))
             self.active_object = 'None'
             self._plugin_manager.send_plugin_message(self._identifier, dict(navBarActive=self.active_object))
+
 
     def process_line(self, line):
         if line.startswith("@"):
@@ -347,7 +361,9 @@ class CancelobjectPlugin(octoprint.plugin.StartupPlugin,
             return cmd
         
         #Check if the cmd is an extrusion move
-        e_move = self.parser.is_extrusion_move(cmd)
+        e_move = None
+        if not self.skipping:
+            e_move = self.parser.is_extrusion_move(cmd)
             
         if cmd == "M82":
             self.trackE = True
@@ -390,12 +406,19 @@ class CancelobjectPlugin(octoprint.plugin.StartupPlugin,
             obj = self._get_entry(self.active_object)
             if obj:
                 #min max X, Y position
-                obj["max_x"] = e_move[0] if e_move[0] > obj["max_x"] else obj["max_x"]
-                obj["max_y"] = e_move[1] if e_move[1] > obj["max_y"] else obj["max_y"]
-                obj["min_x"] = e_move[0] if e_move[0] < obj["min_x"] else obj["min_x"]
-                obj["min_y"] = e_move[1] if e_move[1] < obj["min_y"] else obj["min_y"]
-                self._updateobjects()
-                #print(obj)  
+                if e_move[0] > obj["max_x"]:
+                    obj["max_x"] = e_move[0]
+                if e_move[1] > obj["max_y"]:
+                    obj["max_y"] = e_move[1]
+                if e_move[0] < obj["min_x"]:
+                	obj["min_x"] = e_move[0]
+                if e_move[1] < obj["min_y"]: 
+                    obj["min_y"] = e_move[1]
+                  
+                #obj["max_x"] = (obj["max_x"] + e_move[0])/2
+                #obj["max_y"] = (obj["max_y"] + e_move[1])/2
+                #self._updateobjects()
+                #print(e_move[2])  
         return cmd
 
     def get_update_information(self):
--- octoprint_cancelobject/static/js/cancelobject.js
+++ octoprint_cancelobject/static/js/cancelobject.js
@@ -5,13 +5,18 @@
  * License: AGPLv3
  */
 $(function() {
+    var $cancelOverlay = $('<canvas id="gcode_canvas_cancel_overlay">');
+    var cancelOverlayContext = $cancelOverlay[0].getContext('2d');
+
+    
     function CancelobjectViewModel(parameters) {
     	var PLUGIN_ID = "cancelobject";
         var self = this;
         
+        
         self.global_settings = parameters[0];
         self.loginState = parameters[1];
-        
+        self.cancel_mode = false;
         self.navBarActive = ko.observable();
         self.ActiveID = ko.observable();
         self.ObjectList = ko.observableArray();
@@ -31,7 +36,7 @@ $(function() {
             self.allowed = self.settings.allowed;
             self.shownav = self.settings.shownav;
         };
-
+        
         self.addRegex = function() {
             self.object_regex.push({objreg : ""});
         };
@@ -40,6 +45,11 @@ $(function() {
             self.object_regex.remove(regex)
         };
 
+        self.onStartupComplete = function() {
+             addCanvasOverlays();
+             CancelButtons();
+        }
+        
         self.onSettingsBeforeSave = function () {
 
             self.global_settings.settings.plugins.cancelobject.object_regex(self.object_regex.slice(0));
@@ -56,8 +66,42 @@ $(function() {
 				}),
 				contentType: "application/json; charset=UTF-8"
 			});	
+			
+			self.updateObjects();
         }
-
+        
+        self.updateObjects = function() {
+        
+        	$.ajax({
+				url: API_BASEURL + "plugin/cancelobject",
+				type: "POST",
+				dataType: "json",
+				data: JSON.stringify({
+					command: "objlist"
+				}),
+				contentType: "application/json; charset=UTF-8"
+			});
+        	
+        }
+        
+        self.resetObjects = function() {
+        
+        	$.ajax({
+				url: API_BASEURL + "plugin/cancelobject",
+				type: "POST",
+				dataType: "json",
+				data: JSON.stringify({
+					command: "resetpos"
+				}),
+				contentType: "application/json; charset=UTF-8"
+			});
+			//Clear our canvas
+        	var ctx = cancelOverlayContext;
+            ctx.save();
+            clearContext(ctx);
+            ctx.restore();
+        }
+        
         self.updateObjectList = function() {
         	
         	//start a new entry, don't yet know if this is something that can be done with jinja2
@@ -83,11 +127,15 @@ $(function() {
         			if (self.ObjectList[i]["cancelled"]) { cancelbutton.disabled = true; }
         			entry.appendChild(cancelbutton);
         			entries.appendChild(entry);
+        			
+        			
         		}
+
         	}
         	else { canceltable.innerHTML = "Object list populated when GCODE file is loaded"; }
-        	
         	canceltable.appendChild(entries);
+        	
+        	
         
         }
         
@@ -101,23 +149,30 @@ $(function() {
         	var entry = document.getElementById("entry"+self.ActiveID);
         	entry.className = "entry activeobject";
         }
-        //Seems I can only bind this to document?
-        $(document.body).on("click", ".cancel-btn", function (event) {
-            console.log($(this).attr("value"));
-            var thebutton = $(this);
-            var theobject = $(this).attr("value");
+        
+        function confirm_cancel_button(event) {
+        	console.log(event.target);
+            var theobject = event.target.value;
+            confirm_cancel(theobject);
+        }
+        
+        function confirm_cancel(objid) {
             showConfirmationDialog({
-                        title: gettext("Are you sure?"),
-                        message: gettext("<p><strong>You are about to cancel this object.</strong>"),
-                        question: gettext("Are you sure you want to do this?"),
-                        cancel: gettext("Exit"),
-                        proceed: gettext("Yes, Cancel It"),
-                        onproceed:  function() {
-                        	thebutton.attr("disabled", "disabled");
-                            self.cancelObject(thebutton.attr("value"));
+                title: gettext("Are you sure?"),
+                message: gettext("<p><strong>You are about to cancel this object.</strong>"),
+                question: gettext("Are you sure you want to do this?"),
+                cancel: gettext("Exit"),
+                proceed: gettext("Yes, Cancel It"),
+                onproceed:  function() {
+                        	//thebutton.attr("disabled", "disabled");
+                            self.cancelObject(objid);
                         }
               });
-    	});
+        }
+        
+        //Move this to its own function so we can use it with gcodeviewer
+        $(document.body).on("click", ".cancel-btn", confirm_cancel_button);
+        
     	
         self.onDataUpdaterPluginMessage = function (plugin, data) {          
         	if (data.navBarActive) {
@@ -134,8 +189,236 @@ $(function() {
             	self.updateObjectList();
             }
         }
+//Everything below here is borrowed heavily from briancfisher's ExcludeRegion plugin
+
+    function CancelButtons() {
+      // Don't create buttons if using TouchUI, since they don't work anyway
+      if (self.touchui && self.touchui.isActive())
+        return;
+      
+      if (!$("#gcode_cancel_controls").length) {
+        $("#canvas_container").after(
+          '<div id="gcode_cancel_controls">'+
+            '<div class="main">'+
+              gettext("Cancel Objects")+
+              ' <div class="btn-group action-buttons">'+
+                '<div class="btn btn-mini disabled refreshCO" title="'+ gettext("Refresh object markers") +'">'+
+                  '<i class="fa"></i>'+ gettext("Refresh objects") +'</div>'+
+                '<div class="btn btn-mini disabled resetCO" title="'+ gettext("Reset objects, requires printing to recalculate") +'">'+
+                  '<i class="fa"></i>'+ gettext("Reset object") +'</div>'+
+                '<div class="btn btn-mini disabled cancelCO" title="'+ gettext("Choose object to cancel") +'">'+
+                  '<i class="fa"></i>'+ gettext("Cancel Objects") +'</div>'+
+              '</div>'+
+            '</div>'+
+          '</div>'
+        );
+
+        // Edit button click event
+        self.$cancelButtons = $("#gcode_cancel_controls .btn");
+
+        self.$cancelButtons.click(function() {
+          var $button = $(this);
+
+          // Blur self
+          $button.blur();
+
+          // Check if button is not disabled
+          if (!$button.hasClass("disabled")) {
+            if ($button.hasClass("refreshCO")) {
+              self.updateObjects();
+              rendercancelOverlay();
+            } else if ($button.hasClass("resetCO")) {
+              self.resetObjects();
+              self.updateObjects();
+            } else if ($button.hasClass("cancelCO")) {
+            	if (!self.cancel_mode) {
+            	    startCancelMode();
+              	}
+              	else { endCancelMode(); }
+            }
+          }
+        });
+        
+        enableCancelButtons(true);
+      }
+    }
+    function startCancelMode() {
+        var $gcodeCanvas = $("#gcode_canvas");
+        $gcodeCanvas.on("click", check_point);
+       
     }
     
+    function endCancelMode() {
+        var $gcodeCanvas = $("#gcode_canvas");
+        $gcodeCanvas.off("click", check_point);
+    }
+    
+    function removeCancelButtons() {
+      $("#gcode_cancel_controls").remove();
+      delete self.$cancelButtons;
+    }
+
+    function enableCancelButtons(enabled) {
+    
+      if (self.$cancelButtons) {
+        if (enabled) {
+          self.$cancelButtons.removeClass("disabled");
+        } else {
+          self.$cancelButtons.addClass("disabled");
+        }
+      }
+      
+    }
+
+    function addCanvasOverlays() {
+      if ($("#canvas_container").find(".gcode_canvas_wrapper").length == 0) {
+        var $gcodeCanvas = $("#gcode_canvas");
+        var $wrapper = $('<div class="gcode_canvas_wrapper"></div>');
+        $gcodeCanvas[0].parentNode.insertBefore($wrapper[0], $gcodeCanvas[0]);
+        $wrapper.append($gcodeCanvas);
+        cloneNodeSize($gcodeCanvas[0], $wrapper[0]);
+        appendOverlay($wrapper, $cancelOverlay, $gcodeCanvas);
+        
+      }
+    }
+    
+   function check_point(event) {
+   	  //assume all circular
+   	  console.log("Checking point");
+   	  var pt = eventPositionToCanvasPt(event)
+   	  
+   	  if (self.ObjectList.length > 0) {    
+            for (var i = 0; i < self.ObjectList.length; i++) {
+        		if (self.ObjectList[i]["ignore"]) { continue; }
+        		var px = (self.ObjectList[i]["max_x"] + self.ObjectList[i]["min_x"])/2
+        		var py = (self.ObjectList[i]["max_y"] + self.ObjectList[i]["min_y"])/2
+                var r = 4;
+                var check = Math.hypot(px - pt.x, py - pt.y);
+                //console.log(check);
+                
+                if (check <= r) { 
+                    confirm_cancel(self.ObjectList[i]["id"]);
+                    break;
+                }
+
+             }
+        }	
+      
+    }
+    
+    var pixelRatio = window.devicePixelRatio || 1;
+    function eventPositionToCanvasPt(event) {
+      var canvas = $cancelOverlay[0];
+      var x = (event.offsetX !== undefined ? event.offsetX : (event.pageX - canvas.offsetLeft));
+      var y = (event.offsetY !== undefined ? event.offsetY : (event.pageY - canvas.offsetTop));
+      var pt = transformedPoint(x * pixelRatio, y * pixelRatio);
+      return pt;
+    }
+
+    function appendOverlay($parent, $overlay, $canvas) {
+      $parent.append($overlay);
+      cloneNodeSize($canvas[0], $overlay[0]);
+    }
+    
+    var startupComplete = false;
+    var gcodeViewerPollingComplete = false;
+    function initializeControlsIfReady() {
+      if (startupComplete && gcodeViewerPollingComplete) {
+        if (self.loginState.loggedIn()) {
+          addRefreshButton();
+        }
+      }
+    }    
+
+    function clearContext(ctx) {
+      var p1 = transformedPoint(0, 0);
+      var p2 = transformedPoint(ctx.canvas.width, ctx.canvas.height);
+      ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+    }
+    
+    function cloneNodeSize(fromNode, toNode) {
+      toNode.style.height = fromNode.style.height || fromNode.width+"px";
+      toNode.style.width = fromNode.style.width || fromNode.height+"px";
+      if ((toNode.width !== undefined) && (fromNode.width !== undefined)) {
+        toNode.width = fromNode.width;
+        toNode.height = fromNode.height;
+      }
+    }
+
+    var renderFrameCallbacks;
+    
+    function renderFrame(id, callback /*, ...args */) {
+      if (renderFrameCallbacks == null) {
+        renderFrameCallbacks = {};
+
+        requestAnimationFrame(function() {
+          var callbacks = renderFrameCallbacks;
+          renderFrameCallbacks = null;
+
+          for (var id in callbacks) {
+            var cb = callbacks[id];
+            cb[0].apply(null, cb[1]);
+          }
+        });
+      }
+
+      if (!renderFrameCallbacks[id]) {
+        renderFrameCallbacks[id] = [ callback, Array.prototype.slice.call(arguments, 2) ];
+      }
+    }
+    
+    function rendercancelOverlay() {
+      renderFrame("cancelOverlay",the_callback);
+    }
+    
+    function the_callback() {
+        var ctx = cancelOverlayContext;
+        ctx.save();
+        clearContext(ctx);       
+        if (self.ObjectList.length > 0) {    
+            for (var i = 0; i < self.ObjectList.length; i++) {
+        		//Ignore entries that are just there for functional purposes
+        		if (self.ObjectList[i]["ignore"]) { continue; }
+        		ctx.beginPath();
+        		var px = (self.ObjectList[i]["max_x"] + self.ObjectList[i]["min_x"])/2
+        		var py = (self.ObjectList[i]["max_y"] + self.ObjectList[i]["min_y"])/2
+        		//var px = (ObjectList[i]["max_x"])
+        		//var py = (ObjectList[i]["max_y"])
+        		ctx.fillStyle = "red";
+        		ctx.arc(px, py, 4, 0, 2 * Math.PI);
+        		ctx.fill()
+        	    //ctx.drawImage(cancel_image,px-2, py-2, 4, 4);
+        	    //console.log(ObjectList[i]["id"],ObjectList[i]["max_x"], ObjectList[i]["min_x"],ObjectList[i]["max_y"], ObjectList[i]["min_y"]);
+             }
+        }			        			
+        ctx.restore();
+    }
+
+    var gcodeViewerPollFn = function() {
+      if (!GCODE || !GCODE.renderer || !GCODE.renderer.getOptions().hasOwnProperty('onViewportChange')) {
+        setTimeout(gcodeViewerPollFn, 10);
+        return;
+      }
+    }
+    
+    var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+    var pt  = svg.createSVGPoint();
+    overlayXform = svg.createSVGMatrix();
+    
+    function transformedPoint(x,y) {
+        pt.x=x; pt.y=y;
+        return pt.matrixTransform(overlayXform.inverse());
+    }
+    GCODE.renderer.setOption({
+        onViewportChange: function(xform) {
+          overlayXform = xform;
+          cancelOverlayContext.setTransform(xform.a, xform.b, xform.c, xform.d, xform.e, xform.f);
+          rendercancelOverlay();
+      
+    },
+    });
+}
+ 
     // This is how our plugin registers itself with the application, by adding some configuration
     // information to the global variable OCTOPRINT_VIEWMODELS
     OCTOPRINT_VIEWMODELS.push([
@@ -145,7 +428,7 @@ $(function() {
         // This is a list of dependencies to inject into the plugin, the order which you request
         // here is the order in which the dependencies will be injected into your view model upon
         // instantiation via the parameters argument
-        ["settingsViewModel","loginStateViewModel"],
+        ["settingsViewModel","loginStateViewModel","gcodeViewModel"],
 
         // Finally, this is the list of selectors for all elements we want this view model to be bound to.
         
